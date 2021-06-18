2c0b58810e50239e6a6ba80ab1cda1e3d1b3ceea
FroskyLT <kinglync1@gmail.com>
2021-06-18 16:51:50 +0300

    Updates

---
+++ precache-manifest.bb340b32e86ff550a3d41b5178a282d1.js
@@ -0,0 +1,294 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a644bcd2078f584441833dbc4552c8",
+    "url": "/Academy-Devbridge-Project/index.html"
+  },
+  {
+    "revision": "e29ea72b416ac3c19a2b",
+    "url": "/Academy-Devbridge-Project/static/css/2.058fa3a9.chunk.css"
+  },
+  {
+    "revision": "c52bfdf604ae2a698517",
+    "url": "/Academy-Devbridge-Project/static/css/main.b7558746.chunk.css"
+  },
+  {
+    "revision": "e29ea72b416ac3c19a2b",
+    "url": "/Academy-Devbridge-Project/static/js/2.81463a41.chunk.js"
+  },
+  {
+    "revision": "dbd350d792e705bcad08a655dbc24cf4",
+    "url": "/Academy-Devbridge-Project/static/js/2.81463a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c52bfdf604ae2a698517",
+    "url": "/Academy-Devbridge-Project/static/js/main.e4388b1d.chunk.js"
+  },
+  {
+    "revision": "21ca3788cf6f798ee06f",
+    "url": "/Academy-Devbridge-Project/static/js/runtime-main.8105a907.js"
+  },
+  {
+    "revision": "c86f0cd97ad7ba1ad8c39c48d2853e3c",
+    "url": "/Academy-Devbridge-Project/static/media/available.c86f0cd9.svg"
+  },
+  {
+    "revision": "a6ff0eb676e4b400fc33af8ee1111e42",
+    "url": "/Academy-Devbridge-Project/static/media/availableProduct.a6ff0eb6.svg"
+  },
+  {
+    "revision": "bbbfc4fbaa77f056af222b3be6ec3beb",
+    "url": "/Academy-Devbridge-Project/static/media/book.bbbfc4fb.svg"
+  },
+  {
+    "revision": "921e49351732ad337749619d7981b53f",
+    "url": "/Academy-Devbridge-Project/static/media/bookmarkIcon.921e4935.svg"
+  },
+  {
+    "revision": "ab51a4819a5b662fa231bb9c319178ec",
+    "url": "/Academy-Devbridge-Project/static/media/brunch.ab51a481.svg"
+  },
+  {
+    "revision": "e0119df5a4c1abc874bf0dd7f9b3d453",
+    "url": "/Academy-Devbridge-Project/static/media/burger.e0119df5.svg"
+  },
+  {
+    "revision": "940a8ad4e6ef66b95a1e1cb6d21b7e7b",
+    "url": "/Academy-Devbridge-Project/static/media/buttonArrow.940a8ad4.svg"
+  },
+  {
+    "revision": "6eda175f2f41cc1b2ccde40165a85a46",
+    "url": "/Academy-Devbridge-Project/static/media/calendar.6eda175f.svg"
+  },
+  {
+    "revision": "543d9dcdbd460a537510f69157fc66bc",
+    "url": "/Academy-Devbridge-Project/static/media/cancel.543d9dcd.svg"
+  },
+  {
+    "revision": "15173e6416e225af018d1c86c9e09b84",
+    "url": "/Academy-Devbridge-Project/static/media/check.15173e64.svg"
+  },
+  {
+    "revision": "fa143aff4385f6488b4f6b1c5899e3ce",
+    "url": "/Academy-Devbridge-Project/static/media/checkedNotificationBell.fa143aff.svg"
+  },
+  {
+    "revision": "1f01d0407a241fdb9063d8c478b33af2",
+    "url": "/Academy-Devbridge-Project/static/media/chevronRightIcon.1f01d040.svg"
+  },
+  {
+    "revision": "dc984f98bdda485d326648391aff5a32",
+    "url": "/Academy-Devbridge-Project/static/media/clock.dc984f98.svg"
+  },
+  {
+    "revision": "862ae820aa9c579f9f745015a8cb8918",
+    "url": "/Academy-Devbridge-Project/static/media/cloud.862ae820.svg"
+  },
+  {
+    "revision": "2127d1698b3acdf7be84184ca5059648",
+    "url": "/Academy-Devbridge-Project/static/media/commentBtn.2127d169.svg"
+  },
+  {
+    "revision": "7149888fbae941a3782a6a8d2a75c4e9",
+    "url": "/Academy-Devbridge-Project/static/media/compassIcon.7149888f.svg"
+  },
+  {
+    "revision": "5192f63fb6b302cb472baf8aea3db179",
+    "url": "/Academy-Devbridge-Project/static/media/confetti.5192f63f.svg"
+  },
+  {
+    "revision": "f0d6f054585aebb477908827aadfc82a",
+    "url": "/Academy-Devbridge-Project/static/media/confettiCircle.f0d6f054.svg"
+  },
+  {
+    "revision": "34dea033d9f48e8cd1e13312b501be43",
+    "url": "/Academy-Devbridge-Project/static/media/confettiStar.34dea033.svg"
+  },
+  {
+    "revision": "8d77bcd9652d78719970fbaf82133891",
+    "url": "/Academy-Devbridge-Project/static/media/door.8d77bcd9.svg"
+  },
+  {
+    "revision": "fba3bb95935ebdf9277be48f6e19d283",
+    "url": "/Academy-Devbridge-Project/static/media/dropdownArrow.fba3bb95.svg"
+  },
+  {
+    "revision": "cc2872391a51d0bcf814d4085f51b0d8",
+    "url": "/Academy-Devbridge-Project/static/media/ellipseGrey.cc287239.svg"
+  },
+  {
+    "revision": "f5759a091bf54c6f3118a073efcd7c59",
+    "url": "/Academy-Devbridge-Project/static/media/formLogo.f5759a09.svg"
+  },
+  {
+    "revision": "0a59f3cac180cd89cc32b6058cc6cf3c",
+    "url": "/Academy-Devbridge-Project/static/media/globe.0a59f3ca.svg"
+  },
+  {
+    "revision": "b5d4cd1e3e5e36f5de5edd7458fa4555",
+    "url": "/Academy-Devbridge-Project/static/media/globe.b5d4cd1e.svg"
+  },
+  {
+    "revision": "0a63b966161e1b691d7427e830dc9228",
+    "url": "/Academy-Devbridge-Project/static/media/grill.0a63b966.svg"
+  },
+  {
+    "revision": "9252c169132ddc33e94378b4c1f3d515",
+    "url": "/Academy-Devbridge-Project/static/media/headerFullLogo.9252c169.svg"
+  },
+  {
+    "revision": "d187ffd11cfca6a5c317df3a0557769a",
+    "url": "/Academy-Devbridge-Project/static/media/headerLogo.d187ffd1.svg"
+  },
+  {
+    "revision": "4d519e4033178d01441898684f184fed",
+    "url": "/Academy-Devbridge-Project/static/media/heartBtn.4d519e40.svg"
+  },
+  {
+    "revision": "8111324cffebd8473f24f18ae066f576",
+    "url": "/Academy-Devbridge-Project/static/media/heartBtnBold.8111324c.svg"
+  },
+  {
+    "revision": "584a6268f565c2c05c0de5c57bf04443",
+    "url": "/Academy-Devbridge-Project/static/media/heartBtnColored.584a6268.svg"
+  },
+  {
+    "revision": "42580e14236daa9320e4808191576e22",
+    "url": "/Academy-Devbridge-Project/static/media/homeIcon.42580e14.svg"
+  },
+  {
+    "revision": "984cf5f027caa25e8340811ac0b08190",
+    "url": "/Academy-Devbridge-Project/static/media/kebab.984cf5f0.svg"
+  },
+  {
+    "revision": "89cc5eb5c6a55d23460b0097483b119c",
+    "url": "/Academy-Devbridge-Project/static/media/logoutIcon.89cc5eb5.svg"
+  },
+  {
+    "revision": "a061c890c05e53967ff57ddf9524093a",
+    "url": "/Academy-Devbridge-Project/static/media/mapPin.a061c890.svg"
+  },
+  {
+    "revision": "c80f2c09baf0ed576513e45200c7b53a",
+    "url": "/Academy-Devbridge-Project/static/media/mapPin.c80f2c09.svg"
+  },
+  {
+    "revision": "742b0906eef119f71eb07e1bc5ae85b0",
+    "url": "/Academy-Devbridge-Project/static/media/notAvailableProduct.742b0906.svg"
+  },
+  {
+    "revision": "bd444c9f5ed2ee351efafd8cf83706ec",
+    "url": "/Academy-Devbridge-Project/static/media/notification.bd444c9f.svg"
+  },
+  {
+    "revision": "75b38aec7d7e0285ba535da8229c4b57",
+    "url": "/Academy-Devbridge-Project/static/media/openPresent.75b38aec.svg"
+  },
+  {
+    "revision": "11af1007292d1ee19811285818f2982a",
+    "url": "/Academy-Devbridge-Project/static/media/pancakes.11af1007.svg"
+  },
+  {
+    "revision": "96fc949fb095dfbd115d92e20dcfd033",
+    "url": "/Academy-Devbridge-Project/static/media/phone.96fc949f.svg"
+  },
+  {
+    "revision": "d7970cd3c2e4eb1c9011a0ad58119787",
+    "url": "/Academy-Devbridge-Project/static/media/phone.d7970cd3.svg"
+  },
+  {
+    "revision": "066b14843e46c58cdb87429a929e1bfc",
+    "url": "/Academy-Devbridge-Project/static/media/pizza.066b1484.svg"
+  },
+  {
+    "revision": "6e561bdbba8f02b6131cd9336214547f",
+    "url": "/Academy-Devbridge-Project/static/media/playIcon.6e561bdb.svg"
+  },
+  {
+    "revision": "894eb2a28593a9bf95e3e71dde91c9b6",
+    "url": "/Academy-Devbridge-Project/static/media/presentBtn.894eb2a2.svg"
+  },
+  {
+    "revision": "3a1b36bc5686bc1c5c36c289c881008a",
+    "url": "/Academy-Devbridge-Project/static/media/presentBtnColored.3a1b36bc.svg"
+  },
+  {
+    "revision": "67b54a711956944b6ec8f20070e24657",
+    "url": "/Academy-Devbridge-Project/static/media/progressBar.67b54a71.svg"
+  },
+  {
+    "revision": "79fa427bafdec0253f0f2d5b770c786f",
+    "url": "/Academy-Devbridge-Project/static/media/rain.79fa427b.svg"
+  },
+  {
+    "revision": "30dc4195276ee89977734573245cd280",
+    "url": "/Academy-Devbridge-Project/static/media/rainTear.30dc4195.svg"
+  },
+  {
+    "revision": "91b61f5f8558500581cf14adc8f471ad",
+    "url": "/Academy-Devbridge-Project/static/media/ramen.91b61f5f.svg"
+  },
+  {
+    "revision": "c86f580010309e44b1b5d2fc94b67768",
+    "url": "/Academy-Devbridge-Project/static/media/redCrossIcon.c86f5800.svg"
+  },
+  {
+    "revision": "fbef27a36559f960463e182c8506cd0b",
+    "url": "/Academy-Devbridge-Project/static/media/salads.fbef27a3.svg"
+  },
+  {
+    "revision": "f8479cb3cc15e0c9f4a25b8b2f6590bc",
+    "url": "/Academy-Devbridge-Project/static/media/sandwich.f8479cb3.svg"
+  },
+  {
+    "revision": "1729bc01023d16505b142a1f5da85086",
+    "url": "/Academy-Devbridge-Project/static/media/searchIcon.1729bc01.svg"
+  },
+  {
+    "revision": "883fa7b7b96370eca2b464be4d6a4b75",
+    "url": "/Academy-Devbridge-Project/static/media/settingsIcon.883fa7b7.svg"
+  },
+  {
+    "revision": "d6514a582a48e44d867e4122e643ceef",
+    "url": "/Academy-Devbridge-Project/static/media/snow.d6514a58.svg"
+  },
+  {
+    "revision": "8b16dc0a06a4c70950fa31df076bdad5",
+    "url": "/Academy-Devbridge-Project/static/media/soups.8b16dc0a.svg"
+  },
+  {
+    "revision": "83aca71a118fc3606a450f2718399690",
+    "url": "/Academy-Devbridge-Project/static/media/starEmpty.83aca71a.svg"
+  },
+  {
+    "revision": "ac45ec1a572b0b824f46b93aedf4653f",
+    "url": "/Academy-Devbridge-Project/static/media/starFilled.ac45ec1a.svg"
+  },
+  {
+    "revision": "8a49d203cd7b7e17c6b7ffde859f9274",
+    "url": "/Academy-Devbridge-Project/static/media/sun.8a49d203.svg"
+  },
+  {
+    "revision": "18597522055c13832c3ee7dafb060c72",
+    "url": "/Academy-Devbridge-Project/static/media/sushi.18597522.svg"
+  },
+  {
+    "revision": "f84830da091645018a3c51336a24e4fa",
+    "url": "/Academy-Devbridge-Project/static/media/sweets.f84830da.svg"
+  },
+  {
+    "revision": "e4d047bc3f7a68ad86e2a582f8592bd2",
+    "url": "/Academy-Devbridge-Project/static/media/thunderstorm.e4d047bc.svg"
+  },
+  {
+    "revision": "d36323fd3e1dbc0e598c718bc82759bd",
+    "url": "/Academy-Devbridge-Project/static/media/userIcon.d36323fd.svg"
+  },
+  {
+    "revision": "a98cb4dc452a4bddf6fabd16668950c2",
+    "url": "/Academy-Devbridge-Project/static/media/wind.a98cb4dc.svg"
+  },
+  {
+    "revision": "4d43b1e2238c0e1e35775a47188315fd",
+    "url": "/Academy-Devbridge-Project/static/media/x.4d43b1e2.svg"
+  }
+]);
